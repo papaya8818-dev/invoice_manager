b3755bceee1529481086d30a332a3410929faf22
papaya8818 <papaya8818@gmail.com>
2026-07-10 16:13:41 +0900

    請求データ登録機能を追加

--- src/register_invoice.py
+++ src/register_invoice.py
@@ -26,3 +26,29 @@ gc = gspread.authorize(creds)
 spreadsheet = gc.open_by_key("1h1iTqcLn7NHWqEc8Ndjge_pCN4kAiKAtboSyqjg3tGs")
 
 print("Google Sheetsへ接続できました！")
+
+sheet = spreadsheet.sheet1
+
+invoice = {
+    "請求書No": "2608-01",
+    "送付日": "2026/07/10",
+    "支払期限": "2026/08/10",
+    "取引先": "株式会社サンプル",
+    "案件名": "ホームページ更新",
+    "金額": 15000,
+    "入金日": ""
+}
+
+row = [
+    invoice["請求書No"],
+    invoice["送付日"],
+    invoice["支払期限"],
+    invoice["取引先"],
+    invoice["案件名"],
+    invoice["金額"],
+    invoice["入金日"],
+]
+
+sheet.append_row(list(invoice.values()))
+
+print("請求データを登録しました！")
